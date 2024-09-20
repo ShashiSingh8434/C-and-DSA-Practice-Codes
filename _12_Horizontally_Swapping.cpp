3e5bbee2ac85d8d5b8a7d62c20211866c544da9e
Shashi Singh <shashisingh92292@gmail.com>
2024-09-20 23:03:14 +0530

    Day 6

---
+++ _12_Horizontally_Swapping.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string checkOddEven(int num){
+    if(num % 2 == 0 ){
+        return "even";
+    }else{
+        return "odd" ;
+    }
+}
+
+int evenSwapping(int num){
+    string n = to_string(num);
+    string swapped = n;
+    int index = (n.length())/2;
+
+    for(int i=1 ; i <= (n.length())/2 ; i++){
+
+        swapped[index-i] = n[index+i-1];
+        swapped[index+i-1] = n[index-i];
+    
+    }
+
+    num = stoi(swapped);
+    return num;
+}
+
+int oddSwapping(int num){
+    string n = to_string(num);
+    string swapped = n;
+    int index = (n.length())/2 ;
+
+    for(int i=1 ; i <= (n.length())/2 ; i++){
+
+        swapped[index-i] = n[index+i];
+        swapped[index+i] = n[index-i];
+        
+    }
+
+    num = stoi(swapped);
+    return num;
+}
+
+
+int main(){
+    int num;
+    cout <<"Enter the number to swap: ";
+    cin >> num ;
+    int swap;
+    if (checkOddEven(num) == "even"){
+        swap = evenSwapping(num);
+    }else{
+        swap = oddSwapping(num);
+    }
+
+    cout<<"Swapped : "<<swap<<endl;
+
+    system("pause");
+
+    return 0;
+}
