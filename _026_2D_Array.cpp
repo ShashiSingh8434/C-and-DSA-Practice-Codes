0f126074e120163db1eb57370ecf83be92c042e1
Shashi Singh <alphasingh8434@gmail.com>
2025-02-02 15:04:10 +0530

    2D array

---
+++ _026_2D_Array.cpp
@@ -0,0 +1,14 @@
+// For multi dimensional array 
+// formula to get the index of the element in the memory => c*i+j
+
+// c is no of column 
+// i is the row index 
+// j is the column index 
+
+//    [[1,2,3],
+//     [4,5,6],
+//     [7,8,9]]  
+// 
+//   but the array in memory is like => [1,2,3,4,5,6,7,8,9]
+// 
+//   therefor the index of the elements in memory is not same as we do like in matrix format
