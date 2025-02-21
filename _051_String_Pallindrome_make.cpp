9cea10f0c4875c202e6b9eee50d27376f37edfea
Shashi Singh <alphasingh8434@gmail.com>
2025-02-21 15:14:02 +0530

    pallindrome check

---
+++ _051_String_Pallindrome_make.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool checkPallindrome(string s, int i, int j){
+    while(i<=j){
+        if(s[i] != s[j]){
+            return false; 
+        }
+        else{
+            i++;
+            j--;
+        }
+    }
+    return true;
+}
+
+bool validPallindrome(string s){
+    int i=0, j=s.length()-1;
+
+    while(i<=j){
+        if(s[i] != s[j]){
+            return checkPallindrome(s,i,j-1) || checkPallindrome(s,i+1,j);
+        }
+        else{
+            i++;
+            j--;
+        }
+    }
+    return true;
+}
+
+int main(){
+    string str;
+    cout<<"Enter: ";
+    cin>>str;
+
+    cout<< (validPallindrome(str));
+    
+    return 0;
+}
