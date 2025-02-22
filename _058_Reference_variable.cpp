0e4c95868568faccc8a91a84165242f3304daf40
Shashi Singh <alphasingh8434@gmail.com>
2025-02-22 12:34:50 +0530

    .........

--- _058_Reference_variable.cpp
+++ _058_Reference_variable.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void sq(int& a){
+void sq(int& a){ // pass by reference
     a = a*a;
 }
+
+void pointer(int*& ptr){ // passing pointer as a reference
+    *ptr = *ptr +1;
+}
+
 int main(){
     int a = 15;
     int& b = a;
@@ -17,7 +22,14 @@ int main(){
     sq(a);
 
     cout<<a<<endl;
-    cout<<b<<endl  ;
+    cout<<b<<endl;
+
+    int x = 11;
+    int* asdf = &x;
+
+    cout<<x <<endl;
+    pointer(asdf);
+    cout<<x <<endl;
 
     return 0;
 }
