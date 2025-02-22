8673abda898da3b3e3b1fd7d9d162e1ebb6f5c97
Shashi Singh <alphasingh8434@gmail.com>
2025-02-22 12:41:38 +0530

    return by reference

---
+++ _059_Return_by_reference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int* asdf(int*& a){  // here we are returning the value as reference to a address just know that it can be done and is very useful
+    a += 1;
+    return a;
+}
+
+void lol(int* a){
+    asdf(a);
+    cout<<*a;
+}
+
+int main(){
+    int a[5] = {1,2,3,4,5};
+    lol(a);
+
+    return 0;
+}
